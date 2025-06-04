4588f0e8ae229b2be3641644971afe78e437e2fd
rianexe <rian.rodrigues2004@gmail.com>
2025-06-04 00:23:54 -0300

    Update TiposVariaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,35 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Tipos e Variáveis");
+
+        int numero = 1;
+        numero = 5;
+
+        //constante é representada por final, e a convenção é usar letras maiúsculas
+        final double PI = 3.14;         //não pode ser alterada depois de definida
+
+        /* 8 tipos primitivos em Java
+
+         int            -> mais utilizado
+         byte
+         short   
+         long
+         float
+         double         -> mais utilizado
+         boolean
+         char
+
+        ex: int idade;
+        int anoFabricacao = 2021;
+        double salarioMinimo = 2500.00;
+
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 12345678; se começar com zero, terá que colocar outro tipo
+        long cpf = 12345678901L; // o L é necessário para indicar que é um long
+        float pi = 3.14f; // o f é necessário para indicar que é um float
+        double salario = 2500.00; 
+
+         */
     }
 }
