09a21d0caec46b66b39af6377179930765596f99
rianexe <rian.rodrigues2004@gmail.com>
2025-06-07 00:51:28 -0300

    Estudo sobre palavras reservadas em Java

---
+++ palavras-reservadas/PalavarasReservadas/src/Reservadas.java
@@ -0,0 +1,83 @@
+public class Reservadas {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Estudo sobre palavras reservadas em Java");
+
+        /* Palavras reservadas em Java e seus significados:
+
+        controle de pacotes
+            import: importa pacotes ou classes para dentro do código
+            package: especifica a que pacote todas as classes de um arquivo pertencem
+
+        modificadores de acesso
+
+            public: acesso de qualquer classe
+            private: apenas dentro da classe
+            protected: acesso por classes no mesmo pacote e subclasses
+        
+        primitivos
+
+            boolean: verdadeiro ou falso
+            byte: inteiro de 8 bits
+            char: caracter 
+            double: numero de ponto flutuante 64 bits
+            float: ponto flutuante 32 bits
+            int: inteiro
+            long: inteiro de 64 bit
+            shot: inteiro
+            void: indica que o metodo não tem retorno de um valor
+
+        modificador de classes, variaveis ou metodos
+
+            abstract: classe não pode ser instanciada ou método que precisa ser implementado por uma subclasse não abstrata
+            class: especifica classe
+            extends: indica a superclasse que a classe está estendendo
+            final: impossibilita que classe seja estendida, metodo sobrescrito ou que uma variavel seja reinicializada
+            implements: indica as interfaces que uma classe irá implementar
+            interface: especifica interface
+            native: indica que um metodo esta escreito em uma linguagem dependente de plataforma, como o C
+            new: instancia um novo objeto
+            static: faz um metodo ou variavel pertencer a classe ao inves de as instancias
+            strictfp: usado em frente a um metodo ou classe para indicar que os numeros de ponto flutuante seguirao as regras de ponto flutuante em todas as expressoes
+            synchronized: indica que um metodo so póde ser acessado por uma thread de cada vez
+            transite: impede a serializaçao de campos
+            volatile: indica que uma variavel pode ser alterada durante o uso de threads
+
+        controle de fluxo dentro de um bloco de codigo
+
+            break: sai do bloco de codigo em que ele está
+            case: executa um bloco de codigo dependendo do teste do switch
+            continue: pula a execução do codigo que viriar apos essa linha e para a proxima passagem do loop
+            default: executa esse bloco de codigo caso nenhum dos testes de switch-case seja verdadeiro
+            do: executa um bloco de codigo uma vez, e entao realiza um teste em conjunto com o while para determinar se o bloco devera ser executado novamente
+            else: executa um bloco de codigo alternativo caso o teste if seja falso
+            for: usado para realizar um loop condicional de um bloco de codigo
+            if: usado para realizar um teste lógico de verdadeiro ou falso
+            instanceof: determina se um objeto é uma instancia de determinada classe, superclasse ou interface
+            return: retorna de um metodo sem executar qualquer código que venha depois desta linha (tambem pode retornar uma variavel)
+            switch: indica a variavel a ser comparada nas expressoes case
+            while: executa um bloco de codigo repetidamente enquanto a condicao for verdadeira
+
+        tratamento de erros
+
+            assert: testa uma expressao condicional para verificar uma suposicao do programador
+            catch: declara o bloco de codigo usado para tratar uma excecao
+            finally: bloco de codigo, apos um try-catch, que é executado independentemente do fluxo do programa seguido ao lidar com uma excecao
+            throw: usado para passar uma excecao para o metodo que o chamou
+            throws: indica que um metodo pode passar uma excecao para o metodo que o chamou
+            try: bloco de codigo que tentara ser executado, mas que pode causar uma excecao
+
+        variaveis de referencia
+
+            super: superclasse imediata
+            this: instancia atual do objeto
+
+        palavras reservadas nao utilizadas
+
+            const
+            goto
+
+
+         */
+
+    }
+}
