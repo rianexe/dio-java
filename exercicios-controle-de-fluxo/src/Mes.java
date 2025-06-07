e91bf8165ef050b656bf6d23042b85d79d9c9632
rianexe <rian.rodrigues2004@gmail.com>
2025-06-07 01:53:31 -0300

    Estudo com If Else

---
+++ exercicios-controle-de-fluxo/src/Mes.java
@@ -0,0 +1,49 @@
+public class Mes {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Meses do ano");
+
+        //Exercicio com If
+        
+        int mes = 1;
+
+        if (mes == 1){
+            System.out.println("O mês selecionado é janeiro.");
+            System.out.println("Férias!");
+        } else if (mes == 2){
+            System.out.println("O mês selecionado é fevereiro.");
+
+        } else if (mes == 3){
+            System.out.println("O mês selecionado é março.");
+
+        } else if (mes == 4){
+            System.out.println("O mês selecionado é abril.");
+
+        } else if (mes == 5){
+            System.out.println("O mês selecionado é maio.");
+
+        } else if (mes == 6){
+            System.out.println("O mês selecionado é junho.");
+
+        } else if (mes == 7){
+            System.out.println("O mês selecionado é julho.");
+            System.out.println("Férias!");
+
+        } else if (mes == 8){
+            System.out.println("O mês selecionado é agosto.");
+
+        } else if (mes == 9){
+            System.out.println("O mês selecionado é setembro.");
+
+        } else if (mes == 10){
+            System.out.println("O mês selecionado é outubro.");
+
+        } else if (mes == 11){
+            System.out.println("O mês selecionado é novembro.");
+
+        } else if (mes == 12){
+            System.out.println("O mês selecionado é dezembro.");
+            System.out.println("Férias!");
+        }
+
+    }
+}
