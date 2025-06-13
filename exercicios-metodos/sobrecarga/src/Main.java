356e025c687fefc2033b7af95c44da29013eb7aa
rianexe <rian.rodrigues2004@gmail.com>
2025-06-13 01:14:29 -0300

    Exercicio de sobrecarga de metodo

---
+++ exercicios-metodos/sobrecarga/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+    //Quadrilatero
+    System.out.println("Cálculo dos quadriláteros: ");
+    Quadrilatero.area(5);
+    Quadrilatero.area(10d,10d);
+    Quadrilatero.area(10,7,9);
+    Quadrilatero.area(5f,5f);
+
+    }
+}
