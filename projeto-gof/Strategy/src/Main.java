7404316865cb44aa5e9a6c2b55da67ef0f081972
rianexe <rian.rodrigues2004@gmail.com>
2025-07-16 00:48:26 -0300

    Projeto para estudo de Estrategy

---
+++ projeto-gof/Strategy/src/Main.java
@@ -0,0 +1,21 @@
+import one.digitalinnovation.gof.strategy.*;
+
+public class Main {
+    //Teste do projeto: Praticando com Java Puro: Strategy
+    public static void main(String[] args) throws Exception {
+        
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento agressivo = new ComportamentoAgressivo();
+
+        Robo robo = new Robo();
+        robo.setComportamento(normal);
+        robo.mover();
+
+        robo.setComportamento(defensivo);
+        robo.mover();
+
+        robo.setComportamento(agressivo);
+        robo.mover();
+    }
+}
